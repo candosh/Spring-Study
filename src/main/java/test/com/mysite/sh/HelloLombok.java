793bfc13445dab56b73875f5ff43feb12e5be009
candosh <csh200310@naver.com>
2023-07-17 18:23:55 +0900

    add:test

--- src/main/java/test/com/mysite/sh/HelloLombok.java
+++ src/main/java/test/com/mysite/sh/HelloLombok.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Getter
-public class HelloLombok {
+public class HelloLombok{
 
     private final String hello;
     private final int lombok;
--- src/main/java/test/com/mysite/sh/MainController.java
+++ src/main/java/test/com/mysite/sh/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainController {
+public class MainController{
     @GetMapping("/sh")
     @ResponseBody
     public void index(){
